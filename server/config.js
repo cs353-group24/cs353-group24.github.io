ee03a72bd17226723420de7f39f6988a7125bcea
raupova <mokhlaroyim.raupova@soder.com.tr>
2021-05-03 16:08:29 +0300

    M- initial commit

---
+++ server/config.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const {Client} = require('pg');
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json())
+let server = require('http').Server(app);
+
+let port = (process.env.PORT || 8079);
+server.listen(port, () => console.log('listening on port ' + port));
+
+// home router
+app.get('/', function(req, res){
+    return res.status(200).send("CS353");
+});
+
+const client = new Client({
+    host: "163.172.25.36",
+    port: 5432,
+    user: "mohi",
+    password: "mohimohi",
+    database: "hm"
+})
+
+client.on("connect", () =>{
+    console.log("Database connection established.")
+})
+client.connect();
+
+//-------------------- ROUTES-------------------//
+const patientRouter = require('./routes/patientRoute.js');
+app.use('/patient', patientRouter);
---
+++ server/routes/patientRoute.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {Client} = require('pg');
+
+const client = new Client({
+    host: "163.172.25.36",
+    port: 5432,
+    user: "mohi",
+    password: "mohimohi",
+    database: "hm"
+})
+
+/**
+ * This is an example usage, it does not work
+ * */
+router.route('/').get((req, res) =>{
+    //query string
+    let q = `SELECT * FROM Patients`
+    client.query( q, [req.body.id], (err, result) =>{
+        if(err){
+            return res.send(err);
+        }else {
+            res.send(result);
+        }
+    })
+})
