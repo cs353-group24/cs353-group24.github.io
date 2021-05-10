ff3b5fe69808f99799ded5be9825fd8f645872f2
KAAN ATAKAN ARAY <atakanaray@gmail.com>
2021-05-10 11:05:23 +0300

    some more doctor routes

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ let server = require('http').Server(app);
 let port = (process.env.PORT || 8079);
 server.listen(port, () => console.log('listening on port ' + port));
 
+
+
 // home router
 
 app.get('/', function(req, res){
@@ -23,7 +25,7 @@ app.get('/', function(req, res){
     client.query( q, (err, result) =>{
         if(err){
             res.send(err);
-            console.log("safafgag");
+            console.log("hi");
             return;
         }else {
             res.send(result);
@@ -53,19 +55,31 @@ client.connect();
 const patientRouter = require('./routes/patientRoute.js');
 app.use('/patient', patientRouter);
 */
-// for login national_id and password are used
-app.post('/login1', (req,res,next)=>{
-    let q = 'SELECT * FROM person WHERE email=$1'
-    let params =   [req.body.email]
+// for login national_id and email + password are used
 
-    client.query(q, params,(err, result)=>{
+    /*
+       /login_first:
+        required info:
+        {
+            "email": "$",
+            "password": "$"
+        }
+        $ is the required info(s) that will provided by client side,
+        naming conventions presented above should be followed
+     */
+
+app.post('/login_first', (req,res,next)=>{
+    let q = 'SELECT * FROM person WHERE email=$1'
+    let params =   req.body
+
+    client.query(q, [params.email],(err, result)=>{
         if (err){
             return res.send(error);
         }
         else{
             // res.send(result);
            // console.log(JSON.parse(JSON.stringify(result)))
-            if(result.rows[0].password.toString() === req.body.password.toString()){
+            if(result.rows[0].password.toString() === params.password.toString()){
                 return res.status(200).send(result.rows[0])
             }
             else{
@@ -75,11 +89,23 @@ app.post('/login1', (req,res,next)=>{
     });
 });
 
-app.post('/login2', (req,res,next)=>{
-    let q = 'SELECT * FROM person WHERE national_id=$1'
-    let params =   [req.body.national_id]
+/*
+    /login_second:
+     required info:
+     {
+        "national_id": "$"
+     }
+     $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+  */
 
-    client.query(q, params,(err, result)=>{
+
+
+app.post('/login_second', (req,res,next)=>{
+    let q = 'SELECT * FROM person WHERE national_id=$1'
+    let params =   req.body
+
+    client.query(q, [params.national_id],(err, result)=>{
         if (err){
             return res.send(error);
         }
@@ -90,20 +116,48 @@ app.post('/login2', (req,res,next)=>{
 });
 
 
+/*
+    /logout
+    no info required
+ */
 //ask how to add national_id too
 
 app.get('/logout', (req, res)=>{
     res.redirect('/');
 });
 
+
+
+
+
 //sign up
 
-//also two page split up
+
+
+//also two page split up ??
+
+/*
+/signup
+    {
+        "national_id": "$",
+        "name": "$",
+        "surname": "$",
+        "email": "$",
+        "password": "$",
+        "person_type": "$",
+        "phone": "$",
+        "birthday": "$"
+    }
+    $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+
+ */
 app.post('/signup', (req,res)=>{
     let q = `INSERT INTO person (national_id, name, surname, email, password, person_type, phone, birthday) VALUES 
     ($1, $2, $3, $4, $5, $6, $7, $8);`
-    let params = Object.values(req.body)
-    console.log(params)
+    let re = req.body
+    let params = [re.national_id,re.name,re.surname, re.email, re.password, re.person_type, re.phone, re.birthday]
+
     client.query(q, params,(err, result)=>{
         if (err){
             return res.send(error).send({"message": "insertion unsuccessful"});
@@ -113,22 +167,29 @@ app.post('/signup', (req,res)=>{
 })
 
 
-
-//patient routes
-// /patient/:id/...
-
+/* -----------------------------------------------------------------------------------------------------
+    /user_type/:id
+    the id will be passed by the client side always when calling these type of routes
+----------------------------------------------------------------------------------------------------- */
 
 //order by date
 
-// ask what info is necessary in the json
-//homepage all patient apointments
+
+/*
+    /patient/:id/homepage :
+        /patient/$/homepage :
+        info required is the id it will be passed on the URL
+
+    $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
 app.get('/patient/:id/homepage', (req, res) =>{
     let q = ` SELECT appointment_id, date
             FROM appointment 
-            WHERE appointment.patient_id = $1; `
+            WHERE appointment.patient_id = $1 
+            ORDER BY  date DESC ; `
 
     let params = Object.values(req.params)
-    console.log(params)
     client.query(q, params, (err, result) =>{
         if(err){
             return res.send(error).send({"message": "no appointment found"})
@@ -140,6 +201,14 @@ app.get('/patient/:id/homepage', (req, res) =>{
 
 
 //appointments
+/*
+    /patient/:id/appointments :
+        /patient/$/appointments :
+        info required is the id it will be passed on the URL
+
+    $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
 app.get('/patient/:id/appointment', (req,res)=>{
 
     let q = ` SELECT appointment_id, date
@@ -147,7 +216,7 @@ app.get('/patient/:id/appointment', (req,res)=>{
             WHERE appointment.patient_id = $1; `
 
     let params = Object.values(req.params)
-    console.log(params)
+
     client.query(q, params, (err, result) =>{
         if(err){
             return res.send(error).send({"message": "no appointment found"})
@@ -159,37 +228,28 @@ app.get('/patient/:id/appointment', (req,res)=>{
 })
 
 
-// how to do edit
-app.post('/patient/:id/appointment/edit', (req,res)=>{
+//appointments
+/*
+    /patient/:id/appointments :
+        /patient/$/appointments :
+        info required is the id it will be passed on the URL
 
-    let q = ` SELECT appointment_id, date, status
-            FROM appointment 
-            WHERE appointment.patient_id = $1; `
-
-    let params = Object.values(req.params)
-    console.log(params)
-    client.query(q, params, (err, result) =>{
-        if(err){
-            return res.send(error).send({"message": "no appointment found"})
-        }
-        return res.status(200).send(result.rows)
-    })
-
-})
-
+    $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
 
 //appointment cancel
 app.post('/patient/:id/appointment/cancel', (req,res)=>{
 
     let q = ` DELETE FROM appointment WHERE appointment.appointment_id = $1;`
 
-    let params = Object.values(req.params) // will get id
-    console.log(params)
+    let params = Object.values(req.body) // will get id
+
     client.query(q, params, (err, result) =>{
         if(err){
             return res.send(error).send({"message": "cancelation error"})
         }
-        return res.status(200).send({"message": "canceled successfully"})
+        return res.status(200).send({"message": "deleted successfully"})
     })
 
 
@@ -197,15 +257,30 @@ app.post('/patient/:id/appointment/cancel', (req,res)=>{
 
 
 //new appointment
+
+/*
+/patient/:id/appointment/newappointment:
+/patient/$/appointment/newappointment
+    {
+        "appointment_id": "$",
+        "date": "$",
+        "doctor_id": "$"
+
+    }
+    $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+
+ */
 app.post('/patient/:id/appointment/newappointment', (req,res)=>{
 
     let q = ` INSERT INTO appointment (appointment_id, date,  patient_id, doctor_id) VALUES
                ($1, $2,  $3, $4); `
 
-    let params1 = Object.values(req.params) // will give national id
-    let params2 = Object.values(req.body) //
+    let params1 = req.params // will give national id
+    let params2 = req.body //
+    let params = [params1.appointment_id, params1.date, req.params, params2.doctor_id]
 
-    client.query(q, params2.splice(2,0,params1), (err, result) =>{
+    client.query(q, params, (err, result) =>{
         if(err){
             return res.send(error).send({"message": "no appointment found"})
         }
@@ -215,6 +290,9 @@ app.post('/patient/:id/appointment/newappointment', (req,res)=>{
 
 })
 
+/*
+    no info needed
+ */
 //new appointment get departments
 app.get('/patient/:id/appointment/newappointment/departments', (req,res)=>{
 
@@ -231,6 +309,14 @@ app.get('/patient/:id/appointment/newappointment/departments', (req,res)=>{
 
 })
 
+/*
+    /patient/:id/appointment/newappointment/doctor
+    {
+        "department":"$"
+    }
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
 
 //department value will be passed by the client side
 //new appointment get doctor
@@ -238,11 +324,11 @@ app.get('/patient/:id/appointment/newappointment/doctor', (req,res)=>{
 
     let q = ` SELECT *
 
-              FROM doctor, person
+              FROM doctor
 
-              WHERE department = $1 AND person.national_id = doctor.national_id;  `
+              WHERE department = $1 ;  `
 
-    let params = Object.values(req.params)
+    let params = Object.values(req.body)
 
     client.query(q, params, (err, result) =>{
         if(err){
@@ -255,29 +341,6 @@ app.get('/patient/:id/appointment/newappointment/doctor', (req,res)=>{
 })
 
 
-//time will be implemented according to the feedback
-/*
-app.post('/patient/:id/appointment/newappointment/time', (req,res)=>{
-
-    let q = ` INSERT INTO appointment (appointment_id, date,  patient_id, doctor_id) VALUES
-               ($1, $2,  $3, $4) `
-
-    let params1 = Object.values(req.params)
-    let params2 = Object.values(req.body)
-
-    client.query(q, params1.splice(2,0,params2), (err, result) =>{
-        if(err){
-            return res.send(error).send({"message": "no appointment found"})
-        }
-        return res.status(200).send(result.rows)
-    })
-
-
-})
-*/
-
-
-//add routes for necessary operations as well
 
 
 //doctor routes
@@ -286,10 +349,19 @@ app.post('/patient/:id/appointment/newappointment/time', (req,res)=>{
 
 
 //list all the appointments for the entire month
+
+/*
+    /doctor/:id/homepage
+   /doctor/:$/homepage
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
+
 app.get('/doctor/:id/homepage', (req,res)=>{
     let q = ` SELECT *
                FROM appointment
-                WHERE doctor_id = $1 and EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM current_date);`
+                WHERE doctor_id = $1 and EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM current_date)
+                ORDER BY date DESC ;`
     let params = Object.values(req.params)
     console.log(params)
 
@@ -307,6 +379,13 @@ app.get('/doctor/:id/homepage', (req,res)=>{
 //manage days of -> need database changes
 //add off day
 
+/*
+    /doctor/:id/off_days
+   /doctor/:$/off_days
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
+
 app.get('/doctor/:id/off_days', (req,res)=>{
 
     let q = ` SELECT *
@@ -322,14 +401,25 @@ app.get('/doctor/:id/off_days', (req,res)=>{
     })
 })
 
+/*
+    /doctor/:id/create_off_days
+   /doctor/:$/create_off_days
+
+    {
+        "date": "$"
+    }
+
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
 
 app.post('/doctor/:id/create_off_days', (req,res)=>{
 
     let q = `INSERT INTO doctor_off_days (doctor_id, date ) VALUES ($1, $2); `
 
-    let params1 = Object.values(req.body) //date
-    let params2 = Object.values(req.params) //id
-    let params = params2.concat(params1)
+    let params1 = req.body //date
+    let params2 = req.params //id
+    let params = [params1, params2]
     client.query(q, params, (err, result) =>{
         if(err){
             return res.sendStatus(401)
@@ -337,8 +427,19 @@ app.post('/doctor/:id/create_off_days', (req,res)=>{
         return res.status(200).send({"message":"successful insertion"})
     })
 })
-//write sysptoms
-app.post('/doctor/:id/add_symptoms', (req,res)=>{
+
+/*
+/doctor/:id/make_diagnosis
+    {
+        "apt_tracking_no" : "$",
+        "disease_name": "$",
+        "description":"$"
+    }
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
+//Make diagnosis
+app.post('/doctor/:id/make_diagnosis', (req,res)=>{
 
     let q = `INSERT INTO diagnosis (apt_tracking_no, disease_name, description) VALUES ($1, $2, $3)`
     let params = Object.values(req.body)
@@ -350,7 +451,10 @@ app.post('/doctor/:id/add_symptoms', (req,res)=>{
     })
 })
 
-
+/*
+/doctor/:id/get_test_types
+no info required
+ */
 app.get('/doctor/:id/get_test_types',(req,res)=>{
     let q = ` SELECT * FROM test  `
     client.query(q, params, (err, result) =>{
@@ -362,23 +466,55 @@ app.get('/doctor/:id/get_test_types',(req,res)=>{
 })
 
 
+//how doctor will ask tests ??
+//apt_tracking no and testname should be selected
+// laboratorian will be selected randomly a routed needed to pass required laboratorian types ???
+//ask for tests -> will be ssigned to a random lab technician
 app.post('/doctor/:id/ask_for_tests',(req,res)=>{
 
 
-})
-
-
-app.get('/doctor/:id/get_disease_types', (req,res)=>{
 
 })
 
-app.post('/doctor/:id/make_diagnosis', (req,res)=>{
 
+/*
+/doctor/:id/get_disease_names
+no info required
+ */
+app.get('/doctor/:id/get_disease_names', (req,res)=>{
+    let q = `SELECT * FROM diseases `
+    client.query(q, params, (err, result) =>{
+        if(err){
+            return res.sendStatus(401)
+        }
+        return res.status(200).send(result.rows)
+    })
 })
-//ask for tests -> will be ssigned to a random lab technician
 
 
-//diagnose the patient
+/*
+/doctor/:id/insert_patient_symptoms
+    {
+        "apt_tracking_no" : "$",
+        "symptom_name": "$"
+    }
+       $ is the required info(s) that will provided by client side,
+     naming conventions presented above should be followed
+ */
+app.post('/doctor/:id/insert_patient_symptoms', (req,res)=>{
+let q = `INSERT INTO patient_symptoms (apt_tracking_no, symptom_name) VALUES ($1, $2)`
+
+    let params = Object.values(req.body)
+    client.query(q, params, (err,result)=>{
+        if(err){
+            return res.sendStatus(401)
+        }
+        return res.status(200).send({"message":"successful insertion"})
+    })
+})
+
+
+
 
 
 
